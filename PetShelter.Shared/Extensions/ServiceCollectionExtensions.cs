8e8dad8b63aa1b1978f129c8f6f0c7975fb6c430
pmg <adinkov66@gmail.com>
2024-03-20 10:34:13 +0200

    another progress commit

--- PetShelter.Shared/Extensions/ServiceCollectionExtensions.cs
+++ PetShelter.Shared/Extensions/ServiceCollectionExtensions.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using PetShelter.Shared.Attributes;
 
 namespace PetShelter.Shared.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public static void AutoBind(this IService)
+        public static void AutoBind(this IServiceCollection source, params Assembly[] assemblies)
+        {
+            source.Scan(scan => scan.FromAssemblies(assemblies)
+                .AddClasses(classes => classes.WithAttribute<AutoBindAttribute>())
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+        }
     }
 }
---
+++ PetShelter.Shared/PaginationParameters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShelter.Shared
+{
+    public class PaginationParameters
+    {
+        public const int MaxPageSize = 100;
+        public const int DefaultPageSize = 10;
+        public const int DefaultPageNumber = 1;
+
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
